80d1ca32d058209bee548f7a9e3bf87f45d9ec0e
John Solomon <jolomonson@gmail.com>
2022-08-27 00:59:32 +0100

    Updated model fields Successfully

--- recipe/main/models.py
+++ recipe/main/models.py
@@ -7,3 +7,6 @@ class Recipe(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=8)
     image = models.ImageField(upload_to='recipes/')
     date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
