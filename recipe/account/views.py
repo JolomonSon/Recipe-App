5f54cc87e30e0ab89a9fa4ca186b10c2bbeba95d
John Solomon <jolomonson@gmail.com>
2022-08-27 21:26:55 +0100

    Implemented SignUp with CustomUserCreationForm

--- recipe/account/views.py
+++ recipe/account/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
 from django.contrib import messages
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -14,6 +14,6 @@ def register(request):
             return redirect('home')
         else:
             messages.error(request, "Invalid form!\nTry Again.")
-            form = UserCreationForm()
+            form = CustomUserCreationForm()
 
     return render(request, 'register.html', {'form':form})
