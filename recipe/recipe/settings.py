a40cc64b8021fb047d1d8b0f6b2e168237e1e92e
John Solomon <jolomonson@gmail.com>
2022-08-28 22:07:21 +0100

    Resolved static/media files config error

--- recipe/recipe/settings.py
+++ recipe/recipe/settings.py
@@ -119,11 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'BASE_DIR / static'
+STATIC_URL = '/static/'
+STATIC_ROOT = 'images'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'BASE_DIR / recipes'
+MEDIA_ROOT = 'recipes'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -131,6 +131,11 @@ LOGIN_REDIRECT_URL = 'home'
 LOGIN_URL = 'account/login'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Configuring django-jazzmin
 JAZZMIN_SETTINGS = {
@@ -231,7 +236,3 @@ JAZZMIN_UI_TWEAKS = {
     },
 }
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
