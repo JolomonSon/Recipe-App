f3681dd3de79995e7cf5c447e8e0726e2ee719a3
John Solomon <jolomonson@gmail.com>
2022-08-29 17:42:35 +0100

    Added owner field in Recipe model and migrated

--- recipe/main/models.py
+++ recipe/main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Recipe(models.Model):
@@ -6,6 +7,7 @@ class Recipe(models.Model):
     description = models.TextField()
     price = models.DecimalField(decimal_places=2, max_digits=8)
     image = models.ImageField(upload_to='recipes/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
